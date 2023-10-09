9c7f472d3740debe29049c6d84dcb16f5087177c
yashwant162 <yashwantsingh162@gmail.com>
2023-10-09 18:01:43 +0530

    create breed context api

---
+++ src/components/BreedContext.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+// import axios from "axios";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+export const BreedContext = createContext({})
+
+export function BreedContextProvider({children}) {
+
+  
+  const [breed, setBreed] = useState('')
+  
+  useEffect( () => {
+    axios.get("https://dog.ceo/api/breeds/image/random").then(({data}) => {
+      console.log(data.message.split("/")[4])
+      const breedName = data.message.split("/")[4].split("-")[0];
+      console.log("breed from random url:",breedName)
+      setBreed(breedName)
+    })
+  }, [])
+  const updateBreed = (newBreed) => {
+    setBreed(newBreed);
+    console.log("breed in context :",breed)
+  };
+
+  return (
+    <BreedContext.Provider value = {{breed, setBreed, updateBreed}}>
+      {children}
+    </BreedContext.Provider>
+  )
+}
