b2229fdcc9ee367fd36f37dd9af4621e39f12a86
yashwant162 <yashwantsingh162@gmail.com>
2023-10-11 17:46:11 +0530

    update  button css

--- src/components/breedCarousel/BreedCarousel.jsx
+++ src/components/breedCarousel/BreedCarousel.jsx
@@ -3,7 +3,7 @@ import { DogIcon1, DogIcon2 } from "../svg/DogIcon";
 import { useBreed } from "../../context/useBreed";
 export default function BreedCarousel() {
   const { breedsList} = useBreedCarousel();
-  const { updateBreed}  = useBreed()
+  const { updateBreed, breed}  = useBreed()  
   let i = 0;
   return (
        <div className="flex flex-row items-center overflow-hidden overflow-x-auto scrollbar-hidden">
@@ -14,10 +14,11 @@ export default function BreedCarousel() {
             <div
               key={breedName}
               onClick={() => updateBreed(breedName)}
-              className="bg-gray-800 m-4 p-4 rounded-md cursor-pointer text-xl text-gray-300 flex items-center gap-2 w"
+              className={`${ breed === breedName ? 'border-2 border-slate-200' : ''} bg-gray-800 m-4 p-4 rounded-md cursor-pointer text-xl text-gray-300 flex items-center gap-2 
+              border-2 border-gray-800 hover:border-slate-200`}          
             >
               {displayIcon}
-              <div className="">{breedName}</div>
+              <div className="">{breedName.charAt(0).toUpperCase() + breedName.slice(1)}</div>
             </div>
           );
         })}
